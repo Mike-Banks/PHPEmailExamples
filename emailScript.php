a03f720d1fb29a613d867349fdc05e9ba17bebf6
Mike Banks <banksmic@sheridancollege.ca>
2020-02-03 17:15:05 -0500

    Updating the script, cleaning up the comments and changing a couple of things around. Minor changes.

--- emailScript.php
+++ emailScript.php
@@ -5,15 +5,12 @@
 // Display PHP errors
 ini_set( 'display_errors', 1 );
 
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
-
-// Use the PHP Mailer libraries
+// Import PHPMailer classes into the global namespace - must be at the top of the script
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-// Load Composer's autoloader
-require '/var/www/html/resources/vendor/autoload.php';
+// Load Composer's autoloader - TODO: Set path to your atuload.php file
+require '<PATH_TO_DIRECTORY>/vendor/autoload.php';
 
 // Retrieve the POST parameters
 $message = $_POST['message'];
@@ -26,80 +23,83 @@ try {
 
 //-------------------- SERVER SETTINGS --------------------//
     
-    // Set mailer to use SMTP
-    $mail->isSMTP(); 
-    
-    // Enable verbose debug output - set to 0 for final, 2 for debug - 0 avoids user from being able to see any sensitive info
-    $mail->SMTPDebug = 0; 
-                                     
-    // Specify main and backup SMTP servers, in this case, Google's SMTP Server
-    $mail->Host = 'smtp.gmail.com';  
-    
-    // Enable SMTP authentication
-    $mail->SMTPAuth = true;       
-    
-    // SMTP username / password (email will be sent using this Gmail account)
-    $mail->Username = 'YOUR_EMAIL_HERE';     
-    $mail->Password = 'YOU_PASSWORD_HERE';   
-    
-    // Enable TLS encryption
-    $mail->SMTPSecure = 'tls';     
-    
-    // Specify TCP port to connect to 
-    $mail->Port = 587;
+// Set mailer to use SMTP
+$mail->isSMTP(); 
+
+// Enable verbose debug output - set to 0 for final to avoid user from being able to see any sensitive info
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 0; 
+                                    
+// Specify SMTP server, in this case, Google's SMTP Server
+$mail->Host = 'smtp.gmail.com';  
+
+// Enable SMTP authentication
+$mail->SMTPAuth = true;       
+
+// SMTP username / password (email will be sent using this Gmail account)
+$mail->Username = 'YOUR_EMAIL_HERE'; //TODO: Put Email here     
+$mail->Password = 'YOU_PASSWORD_HERE'; //TODO: Put Password here
+
+// Enable TLS encryption
+$mail->SMTPSecure = 'tls';     
+
+// Specify TCP port to connect to 
+$mail->Port = 587;
 
 //-------------------- RECIPIENTS --------------------//
     
-    // NOTE: Names are optional
-    
-    // From Address
-    $mail->setFrom('YOUR_EMAIL_HERE', 'YOUR_NAME_HERE');
-    
-    // Add a Recipient
-    $mail->addAddress('john@example.com', 'John');
-    
-    // Add a Reply-To Address
-    //$mail->addReplyTo('info@example.com', 'Information');
-    
-    // Add a CC Address
-    //$mail->addCC('cc@example.com');
-    
-    // Add a BCC Address
-    //$mail->addBCC('bcc@example.com');
+// NOTE: Names are optional
+
+// From Address
+$mail->setFrom('YOUR_EMAIL_HERE', 'YOUR_NAME_HERE');
+
+// Add a Recipient
+$mail->addAddress('john@example.com', 'John');
+
+// Add a Reply-To Address
+//$mail->addReplyTo('info@example.com', 'Information');
+
+// Add a CC Address
+//$mail->addCC('cc@example.com');
+
+// Add a BCC Address
+//$mail->addBCC('bcc@example.com');
 
 //-------------------- ATTACHMENTS --------------------//
     
-    // NOTE: Names are optional
-    
-    // Add attachment
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');
+// NOTE: Names are optional
+
+// Add attachment
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');
 
 //-------------------- CONTENT --------------------//
     
-    // Set email format to HTML (if true)
-    $mail->isHTML(false); 
-    
-    // Set Subject
-    $mail->Subject = 'This is the Subject';
-    
-    // Set Body, you can append to it if you wish. In this case, the name, email and message of the person who filled out the form 
-    $mail->Body = 'Name: ' . $name . "\r\n";
-    $mail->Body .= 'Email: ' . $email . "\r\n\r\n";
-    $mail->Body .= 'Message: ' . $message;
-    
-    // The body can include HTML if you wish
-    //$mail->Body = 'This is the HTML message body <b>in bold!</b>';
-    
-    // If using HTML, you should set an Alternate Body that will be displayed for non-HTML mail clients
-    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+// Set email format to HTML (if true)
+$mail->isHTML(false); 
+
+// Set Subject
+$mail->Subject = 'This is the Subject';
+
+// Set Body, you can append to it if you wish. In this case, the name, email and message of the person who filled out the form 
+$mail->Body = 'Name: ' . $name . "\r\n";
+$mail->Body .= 'Email: ' . $email . "\r\n\r\n";
+$mail->Body .= 'Message: ' . $message;
+
+// The body can include HTML if you wish
+//$mail->Body = 'This is the HTML message body <b>in bold!</b>';
+
+// If using HTML, you should set an Alternate Body that will be displayed for non-HTML mail clients
+//$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 //-------------------- SEND / VALIDATION --------------------//
     
-    // Send the Email
-    $mail->send();
-    
-    // Echo message if email was sent properly
-    echo 'Message has been sent';
+// Send the Email
+$mail->send();
+
+// Echo message if email was sent properly
+echo 'Message has been sent';
 
 // If there was an exception, catch it, then echo the error
 } catch (Exception $e) {
